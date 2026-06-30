f52156a7a74a872a3987f67313de5429bec69643
Diegorafaelx <capir.2025116isinf0030@aluno.ifpi.edu.br>
2026-06-30 14:35:37 -0300

    Implement color toggle function
    Add function to toggle background and text color.

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,9 @@
-const botao = document.getElementById('botao');
+function mudarCor() {
+    if (document.body.style.backgroundColor === "white") {
+        document.body.style.backgroundColor = "#222"; 
+        document.body.style.color = "white"; 
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "#222"; 
+    }
+}
